2cf23acb49e6fca771ae5e688219816371a90a0a
He <katharine.he@accenture.com>
2023-09-28 13:54:22 +1000

    new stories

---
+++ src/stories/Radio.stories.ts
@@ -0,0 +1,37 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Radio } from '../components/Radio';
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+const meta = {
+  title: 'Components/Radio',
+  component: Radio,
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
+    layout: 'centered',
+  },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
+  tags: ['autodocs'],
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {
+    //backgroundColor: { control: 'color' },
+  },
+} satisfies Meta<typeof Radio>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+/*export const Default: Story = {
+    args: {
+        id: 'Default',
+        status: 'default',
+        label: 'Name: ',
+    },
+}
+
+export const Active: Story = {
+    args: {
+        id: 'Active',
+        status: 'active',
+        label: 'Text: ',
+    },
+}*/
