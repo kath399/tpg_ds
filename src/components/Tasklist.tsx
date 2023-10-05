2dd3769475431dc9a43bac94c2fcd845283a6ab7
He <katharine.he@accenture.com>
2023-10-05 16:18:12 +1100

    Tasks and Tasklist

---
+++ src/components/Tasklist.tsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+//import { updateTaskState } from '../lib/store';
+import { Task } from './Task';
+
+interface TaskListProps {
+  loading: boolean;
+  tasks: Array<{
+    id: number;
+    title: string;
+    state: string;
+  }>;
+  onPinTask: (id: number) => void;
+  onArchiveTask: (id: number) => void;
+}
+
+export const TaskList = ({ loading, tasks, onPinTask, onArchiveTask }: TaskListProps) => {
+  const events = {
+    onPinTask,
+    onArchiveTask,
+  };
+
+  const LoadingRow = (
+    <div className="loading-item">
+      <span className="glow-checkbox" />
+      <span className="glow-text">
+        <span>Loading</span> <span>cool</span> <span>state</span>
+      </span>
+    </div>
+  );
+
+  if (loading) {
+    return (
+      <div className="list-items" data-testid="loading" key={"loading"}>
+        {LoadingRow}
+        {LoadingRow}
+        {LoadingRow}
+        {LoadingRow}
+        {LoadingRow}
+        {LoadingRow}
+      </div>
+    );
+  }
+
+  if (tasks.length === 0) {
+    return (
+      <div className="list-items" key={"empty"} data-testid="empty">
+        <div className="wrapper-message">
+          <span className="icon-check" />
+          <p className="title-message">You have no tasks</p>
+          <p className="subtitle-message">Sit back and relax</p>
+        </div>
+      </div>
+    );
+  }
+
+
+
+  return (
+    <div className="list-items">
+      {tasks.map(task => (
+        <Task key={task.id} task={task} {...events} />
+      ))}
+    </div>
+  );
+};
+
+
+
+
+
