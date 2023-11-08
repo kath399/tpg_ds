a2ebac216a158aa30d850e5d617bea477153f5cb
Eshaa Sood <eshaa.sood@accenture.com>
2023-11-08 14:07:25 +1100

    checkbox styling

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -20,8 +20,8 @@ const config: StorybookConfig = {
   docs: {
     autodocs: "tag",
   },
-  staticDirs: ["..\\public"], 
+  //staticDirs: ["..\\public"], 
   // currently ommitted and will prevent Storybook from serving any static assets
-  //staticDirs: [],
+  staticDirs: [],
 };
 export default config;
