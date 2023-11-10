d7c3118977e0f9ba1959ea9f0f8afc1b289487c7
He <katharine.he@accenture.com>
2023-11-10 20:17:53 +1100

    Automated tests configuration

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -44,6 +44,31 @@ const preview: Preview = {
     docs: {
       
     },
+    a11y: {
+      // Optional selector to inspect
+      element: '#storybook-root',
+      config: {
+        reporter: [
+          
+        ],
+        rules: [
+          {
+            // The autocomplete rule will not run based on the CSS selector provided
+            id: 'autocomplete-valid',
+            selector: '*:not([autocomplete="nope"])',
+          },
+          {
+            // Setting the enabled option to false will disable checks for this particular rule on all stories.
+            id: 'image-alt',
+            enabled: false,
+          },
+        ],
+      },
+      // Axe's options parameter
+      options: {},
+      // Optional flag to prevent the automatic check
+      manual: false,
+    },
   },
 };
 
