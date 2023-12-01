80a3d8e804f619a3454d1b0818ce9b6c9dafbb94
Eshaa Sood <eshaa.sood@accenture.com>
2023-12-01 16:27:56 +1100

    fix compile error

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,10 +1,9 @@
 
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import './button.css';
 
 
 interface ButtonProps {
-  
   
   /**
    * How large should the button be? 
