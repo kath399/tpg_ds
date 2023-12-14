4725bfe262ba1bae27d4f0ce407183f3eecb2422
He <katharine.he@accenture.com>
2023-12-15 10:45:49 +1100

    Typography.mdx and Radio buttons

--- src/components/Radio.tsx
+++ src/components/Radio.tsx
@@ -3,24 +3,56 @@ import './radio.css';
 
 interface RadioProps {
   id: string;
-  label: string;
+  name: string;
+  label: string[];
+  value: string;
+  checked?: boolean;
+  numberOfBtns: number;
+  onChange: (value: string) => void;
+  inline?: boolean;
+  disabled?: boolean;
 }
 
-export const Radio = ({
-  id,
-  label
-}: RadioProps) => {
+export const Radio: React.FC<RadioProps> = ({ 
+  id, 
+  name,
+  label, 
+  value, 
+  checked,
+  numberOfBtns = 5, 
+  onChange,
+  inline = false,
+  disabled = false,
+}) => {
+  const radioButtons = [];
+
+  const containerStyle: React.CSSProperties = {
+    display: inline ? 'inline-block' : 'block',
+    marginRight: inline ? '16px' : '0',
+  }
+
+  for (let i = 0; i < numberOfBtns; i++) {
+    const uniqueId = `${id}_${i}`;
+
+    radioButtons.push(      
+      <label id={uniqueId} className='container' style={containerStyle}>
+        <input
+          type="radio"
+          name={name}
+          value={value}
+          checked={checked}
+          onChange={() => onChange(value)}
+          disabled={disabled}
+        />
+        <span className="checkmark"></span>
+        {label[i]}
+      </label>
+    );
+  }
 
   return (
-    <label id={id} className='container'>
-      <input 
-        id={id}
-        type="radio"
-        className=""
-        checked
-      />
-      <span className="checkmark"></span>{label}
-    </label>
+    <div>
+      {radioButtons}
+    </div>
   );
 };
-  
--- src/stories/Radio.stories.ts
+++ src/stories/Radio.stories.ts
@@ -3,7 +3,7 @@ import { Radio } from '../components/Radio';
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 const meta = {
-  title: 'Core/Radio',
+  title: 'Core/Radio Button',
   component: Radio,
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
@@ -20,16 +20,72 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Selected: Story = {
+export const Checked: Story = {
     args: {
         id: '',
-        label: 'Label'
+        name: 'Button',
+        label: ['Button Label'],
+        checked: true,
+        numberOfBtns: 1,
+        value: '',
+        disabled: false,
+    },
+    parameters: {
+      design: {
+        type: "figma",
+        url: "https://www.figma.com/file/Xl129GKsg3kTgKt6spkUM5/2.1-Web-Core?type=design&node-id=5915-715&mode=design&t=tMCJiBlFOTqX0r9d-4"
+      },
     },
 }
 
-export const Unselected: Story = {
+export const Unchecked: Story = {
   args: {
       id: '',
-      label: 'Label'
+      name: 'Button',
+      label: ['Button Label'],
+      numberOfBtns: 1,
+      value: '',
+      disabled: false,
+  },
+  parameters: {
+    design: {
+      type: "figma",
+      url: "https://www.figma.com/file/Xl129GKsg3kTgKt6spkUM5/2.1-Web-Core?type=design&node-id=5915-721&mode=design&t=tMCJiBlFOTqX0r9d-4"
+    },
+  },
+}
+
+export const Inline: Story = {
+  args: {
+      id: '',
+      name: 'Button',
+      label: ['Button Label 1', 'Button Label 2', 'Button Label 3', 'Button Label 4', 'Button Label 5'],
+      value: '',
+      numberOfBtns: 5,
+      inline: true,
+      disabled: false,
+  },
+  parameters: {
+    design: {
+      type: "figma",
+      url: "https://www.figma.com/file/Xl129GKsg3kTgKt6spkUM5/2.1-Web-Core?type=design&node-id=5945-3821&mode=design&t=tMCJiBlFOTqX0r9d-4"
+    },
+  },
+}
+
+export const Stacked: Story = {
+  args: {
+      id: '',
+      name: 'Button',
+      label: ['Button Label 1', 'Button Label 2', 'Button Label 3', 'Button Label 4', 'Button Label 5'],
+      value: '',
+      numberOfBtns: 5,
+      disabled: false,
+  },
+  parameters: {
+    design: {
+      type: "figma",
+      url: "https://www.figma.com/file/Xl129GKsg3kTgKt6spkUM5/2.1-Web-Core?type=design&node-id=5945-3820&mode=design&t=tMCJiBlFOTqX0r9d-4"
+    },
   },
 }
